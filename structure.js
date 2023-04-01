2c5ada9c99be16570c3c6558e8218f3b875429ef
llizzxx <Parshinaelizaveta@gmail.com>
2023-04-01 18:28:20 +0300

    Завершение работы над экраном сравнения строения

--- structure.js
+++ structure.js
@@ -35,6 +35,10 @@ function set_state_1() {
     $('#img_female').addClass('structure_img_female_1')
     $('#img_male').removeClass('structure_img_male_2 structure_img_male_3')
     $('#img_male').addClass('structure_img_male_1')
+    $('.second_screen').removeClass('second_screen_2 second_screen_3 white_screen')
+    $('.second_screen').addClass('second_screen_1 ligth_blue_screen')
+    $('.structure_text_female').addClass('structure_text_hide')
+    $('.structure_text_male').addClass('structure_text_hide')
 }
 
 function set_state_2() {
@@ -42,6 +46,10 @@ function set_state_2() {
     $('#img_female').addClass('structure_img_female_2')
     $('#img_male').removeClass('structure_img_male_1 structure_img_male_3')
     $('#img_male').addClass('structure_img_male_2')
+    $('.second_screen').removeClass('second_screen_1 second_screen_3 ligth_blue_screen')
+    $('.second_screen').addClass('second_screen_2 white_screen')
+    $('.structure_text_female').removeClass('structure_text_hide')
+    $('.structure_text_male').addClass('structure_text_hide')
 }
 
 function set_state_3() {
@@ -49,4 +57,8 @@ function set_state_3() {
     $('#img_female').addClass('structure_img_female_3')
     $('#img_male').removeClass('structure_img_male_1 structure_img_male_2')
     $('#img_male').addClass('structure_img_male_3')
+    $('.second_screen').removeClass('second_screen_1 second_screen_2 ligth_blue_screen')
+    $('.second_screen').addClass('second_screen_3 white_screen')
+    $('.structure_text_female').addClass('structure_text_hide')
+    $('.structure_text_male').removeClass('structure_text_hide')
 }
