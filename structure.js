82c1d604cff73bb5be7f579d3c9944b3fda5fc7a
llizzxx <Parshinaelizaveta@gmail.com>
2023-04-07 21:17:11 +0300

    Окончание работы над экраном механизмов защиты

--- structure.js
+++ structure.js
@@ -69,19 +69,19 @@ function set_state_3() {
 
 
 $(document).ready(function() {
-    $('.little_white_color_1_position').click(function(event) {
+    $('.little_white_circle_1_position').click(function(event) {
         if(is_state_1_third_screen()) {
             set_state_2_third_screen()
             event.stopPropagation()
         }
     })
-    $('.little_white_color_2_position').click(function(event) {
+    $('.little_white_circle_2_position').click(function(event) {
         if(is_state_1_third_screen()) {
             set_state_3_third_screen()
             event.stopPropagation()
         }
     })
-    $('.little_white_color_3_position').click(function(event) {
+    $('.little_white_circle_3_position').click(function(event) {
         if(is_state_1_third_screen()) {
             set_state_4_third_screen()
             event.stopPropagation()
@@ -95,55 +95,55 @@ $(document).ready(function() {
 })
 
 function is_state_1_third_screen() {
-    return $('.protection_container').hasClass('protection_container_1')
+    return $('.protection_container_background').hasClass('protection_container_background_1')
 }
 
 function is_state_2_third_screen() {
-    return $('.protection_container').hasClass('protection_container_2')
+    return $('.protection_container_background').hasClass('protection_container_background_2')
 }
 
 function is_state_3_third_screen() {
-    return $('.protection_container').hasClass('protection_container_3')
+    return $('.protection_container_background').hasClass('protection_container_background_3')
 }
 
 function is_state_4_third_screen() {
-    return $('.protection_container').hasClass('protection_container_4')
+    return $('.protection_container_background').hasClass('protection_container_background_4')
 }
 
 function set_state_1_third_screen() {
     $('.third_screen').removeClass('third_screen_state_234 white_screen')
     $('.third_screen').addClass('third_screen_state_1 light_blue_screen')
-    $('.little_white_color_position').removeClass('little_white_color_hide')
+    $('.little_white_circle').removeClass('little_white_circle_hide')
     $('.protection_info_1').addClass('protection_info_hide')
     $('.protection_info_2').addClass('protection_info_hide')
     $('.protection_info_3').addClass('protection_info_hide')
-    $('.protection_container').addClass('protection_container_1')
-    $('.protection_container').removeClass('protection_container_2 protection_container_3 protection_container_4')
+    $('.protection_container_background').addClass('protection_container_background_1')
+    $('.protection_container_background').removeClass('protection_container_background_2 protection_container_background_3 protection_container_background_4')
 }
 
 function set_state_2_third_screen() {
     $('.third_screen').removeClass('third_screen_state_1 light_blue_screen')
     $('.third_screen').addClass('third_screen_state_234 white_screen')
-    $('.little_white_color_position').addClass('little_white_color_hide')
+    $('.little_white_circle').addClass('little_white_circle_hide')
     $('.protection_info_1').removeClass('protection_info_hide')
-    $('.protection_container').addClass('protection_container_2')
-    $('.protection_container').removeClass('protection_container_1 rotection_container_3 protection_container_4')
+    $('.protection_container_background').addClass('protection_container_background_2')
+    $('.protection_container_background').removeClass('protection_container_background_1 rotection_container_3 protection_container_background_4')
 }
 
 function set_state_3_third_screen() {
     $('.third_screen').removeClass('third_screen_state_1 light_blue_screen')
     $('.third_screen').addClass('third_screen_state_234 white_screen')
-    $('.little_white_color_position').addClass('little_white_color_hide')
+    $('.little_white_circle').addClass('little_white_circle_hide')
     $('.protection_info_2').removeClass('protection_info_hide')
-    $('.protection_container').addClass('protection_container_3')
-    $('.protection_container').removeClass('protection_container_1 protection_container_2 protection_container_4')
+    $('.protection_container_background').addClass('protection_container_background_3')
+    $('.protection_container_background').removeClass('protection_container_background_1 protection_container_background_2 protection_container_background_4')
 }
 
 function set_state_4_third_screen() {
     $('.third_screen').removeClass('third_screen_state_1 light_blue_screen')
     $('.third_screen').addClass('third_screen_state_234 white_screen')
-    $('.little_white_color_position').addClass('little_white_color_hide')
+    $('.little_white_circle').addClass('little_white_circle_hide')
     $('.protection_info_3').removeClass('protection_info_hide')
-    $('.protection_container').addClass('protection_container_4')
-    $('.protection_container').removeClass('protection_container_1 protection_container_2 protection_container_3')
+    $('.protection_container_background').addClass('protection_container_background_4')
+    $('.protection_container_background').removeClass('protection_container_background_1 protection_container_background_2 protection_container_background_3')
 }
